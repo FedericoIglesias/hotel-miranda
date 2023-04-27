e720db8b96bb86ba42978b314664fb1e30e26098
Federico Iglesias <igl.fede@gmail.com>
2023-04-27 17:28:23 +0200

    transformToTS

--- js/app.js
+++ js/app.js
@@ -1,68 +1,53 @@
-let barMenuBtn = document.getElementById('bar__menuBtn');
-let barMenu = document.getElementById('bar__menu');
-let bar = document.getElementById('bar');
-let menuBtn = document.getElementById('menuBtn');
-
-
-
-
-
+var barMenuBtn = document.getElementById('bar__menuBtn');
+var barMenu = document.getElementById('bar__menu');
+var bar = document.getElementById('bar');
+var menuBtn = document.getElementById('menuBtn');
 // HEADER DESPEGABLE  ----------
-
-barMenuBtn.onclick = function(){
-    console.log('toy en function')
-    if(document.getElementById('menus__bar').getAttribute('class').includes('display--none')){
-        document.getElementById('menus__bar').setAttribute('class','div__menu ')
-    }else{
-        document.getElementById('menus__bar').setAttribute('class','div__menu display--none')
+barMenuBtn.onclick = function () {
+    console.log('toy en function');
+    if (document.getElementById('menus__bar').getAttribute('class').includes('display--none')) {
+        document.getElementById('menus__bar').setAttribute('class', 'div__menu ');
+    }
+    else {
+        document.getElementById('menus__bar').setAttribute('class', 'div__menu display--none');
+    }
+};
+function disableMenu() {
+    document.getElementById('menus__bar').setAttribute('class', 'div__menu display--none');
+}
+window.addEventListener('scroll', disableMenu);
+function enableMenu() {
+    if (scrollY >= 300) {
+        barMenu.setAttribute('class', 'header__div header');
     }
 }
-function disableMenu (){
-    document.getElementById('menus__bar').setAttribute('class','div__menu display--none')
+function disablePosition() {
+    barMenu.setAttribute('class', 'header__div header display--none');
 }
-window.addEventListener('scroll', disableMenu )
-
-function enableMenu (){
-    if(scrollY >= 300){
-        barMenu.setAttribute('class','header__div header')
-    }
-}
-function disablePosition(){
-    barMenu.setAttribute('class','header__div header display--none')
-}
-
-bar.addEventListener('mousemove',enableMenu)
-
-barMenu.addEventListener('mouseout',disablePosition)
-
-
-
+bar.addEventListener('mousemove', enableMenu);
+barMenu.addEventListener('mouseout', disablePosition);
 // MENU DESPEGABLE-------------
-
-menuBtn.onclick = function(){
-    console.log('toy en function')
-    if(document.getElementById('menus').getAttribute('class').includes('display--none')){
-        document.getElementById('menus').setAttribute('class','div__menu ')
-    }else{
-        document.getElementById('menus').setAttribute('class','div__menu display--none')
+menuBtn.onclick = function () {
+    console.log('toy en function');
+    if (document.getElementById('menus').getAttribute('class').includes('display--none')) {
+        document.getElementById('menus').setAttribute('class', 'div__menu ');
     }
+    else {
+        document.getElementById('menus').setAttribute('class', 'div__menu display--none');
+    }
+};
+function disableMenu() {
+    document.getElementById('menus').setAttribute('class', 'div__menu display--none');
 }
-function disableMenu (){
-    document.getElementById('menus').setAttribute('class','div__menu display--none')
-}
-window.addEventListener('scroll', disableMenu )
-
+window.addEventListener('scroll', disableMenu);
 // SWIPER ----------------------------------
-
-
-const swiperRoom = new Swiper('.swiperRoom', {
+var swiperRoom = new Swiper('.swiperRoom', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1,
     edgeSwipeDetection: false,
     effect: 'coverflow',
-
     breakpoints: {
         1000: {
             slidesPerView: 2,
@@ -71,23 +56,18 @@ const swiperRoom = new Swiper('.swiperRoom', {
             centeredSlidesBounds: true,
         },
     },
-
     // Navigation arrows
     navigation: {
         nextEl: '.room-next',
         prevEl: '.room-prev',
     },
-
 });
-
-const swiperRoom3 = new Swiper('.swiperRoom3', {
+var swiperRoom3 = new Swiper('.swiperRoom3', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1,
     edgeSwipeDetection: false,
-    
-
     breakpoints: {
         1000: {
             slidesPerView: 3,
@@ -96,46 +76,35 @@ const swiperRoom3 = new Swiper('.swiperRoom3', {
             centeredSlidesBounds: true,
         },
     },
-
     // Navigation arrows
     navigation: {
         nextEl: '.room-next',
         prevEl: '.room-prev',
     },
-
 });
-
-
-const swiperMenu = new Swiper('.swiperMenu', {
+var swiperMenu = new Swiper('.swiperMenu', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-
     breakpoints: {
         1000: {
             slidesPerView: 2,
             spaceBetween: 0,
             createElement: false,
-            
         },
     },
-
     // Navigation arrows
     navigation: {
         nextEl: '.menu-next',
         prevEl: '.menu-prev',
     },
-
 });
-
-const swiperFacilities = new Swiper('.swiperFacilities', {
+var swiperFacilities = new Swiper('.swiperFacilities', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
     edgeSwipeDetection: false,
     centeredSlides: true,
-
-
     breakpoints: {
         1000: {
             slidesPerView: 6,
@@ -143,21 +112,16 @@ const swiperFacilities = new Swiper('.swiperFacilities', {
             createElement: false,
         },
     },
-
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
-
 });
-
-const swiperImg = new Swiper('.swiperImg', {
+var swiperImg = new Swiper('.swiperImg', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
     edgeSwipeDetection: false,
-
-
     breakpoints: {
         1000: {
             slidesPerView: 3,
@@ -165,25 +129,21 @@ const swiperImg = new Swiper('.swiperImg', {
             createElement: false,
         },
     },
-
     scrollbar: {
         el: '.swiper-scrollbar-first',
     },
 });
-
-const swiperImgTwo = new Swiper('.swiperImgTwo', {
+var swiperImgTwo = new Swiper('.swiperImgTwo', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
     edgeSwipeDetection: false,
-    
     breakpoints: {
-        320:{
+        320: {
             centeredSlides: true,
             slidesPerView: 1,
             spaceBetween: 0,
-            centerInsufficientSlides:true,
-
+            centerInsufficientSlides: true,
         },
         1000: {
             slidesPerView: 2,
@@ -192,11 +152,8 @@ const swiperImgTwo = new Swiper('.swiperImgTwo', {
             createElement: false,
         },
     },
-
-
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
-
 });
---
+++ js/app.ts
@@ -0,0 +1,201 @@
+const barMenuBtn: HTMLElement | null  = document.getElementById('bar__menuBtn');
+const barMenu: HTMLElement | null = document.getElementById('bar__menu');
+const bar: HTMLElement | null = document.getElementById('bar');
+const menuBtn: HTMLElement | null = document.getElementById('menuBtn');
+
+
+
+
+
+// HEADER DESPEGABLE  ----------
+
+barMenuBtn.onclick = function(): void{
+    if(document.getElementById('menus__bar').getAttribute('class').includes('display--none')){
+        document.getElementById('menus__bar').setAttribute('class','div__menu ')
+    }else{
+        document.getElementById('menus__bar').setAttribute('class','div__menu display--none')
+    }
+}
+const  disableMenu = (): void => {
+    document.getElementById('menus__bar').setAttribute('class','div__menu display--none')
+}
+window.addEventListener('scroll', disableMenu )
+
+function enableMenu (): void{
+    if(scrollY >= 300){
+        barMenu.setAttribute('class','header__div header')
+    }
+}
+function disablePosition(): void{
+    barMenu.setAttribute('class','header__div header display--none')
+}
+
+bar.addEventListener('mousemove',enableMenu)
+
+barMenu.addEventListener('mouseout',disablePosition)
+
+
+
+// MENU DESPEGABLE-------------
+
+menuBtn.onclick = function(): void{
+    console.log('toy en function')
+    if(document.getElementById('menus').getAttribute('class').includes('display--none')){
+        document.getElementById('menus').setAttribute('class','div__menu ')
+    }else{
+        document.getElementById('menus').setAttribute('class','div__menu display--none')
+    }
+}
+function disableMenu (): void{
+    document.getElementById('menus').setAttribute('class','div__menu display--none')
+}
+window.addEventListener('scroll', disableMenu )
+
+// SWIPER ----------------------------------
+
+
+const swiperRoom = new Swiper('.swiperRoom', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    edgeSwipeDetection: false,
+    effect: 'coverflow',
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            createElement: false,
+            centeredSlidesBounds: true,
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.room-next',
+        prevEl: '.room-prev',
+    },
+
+});
+
+const swiperRoom3 = new Swiper('.swiperRoom3', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    edgeSwipeDetection: false,
+    
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+            createElement: false,
+            centeredSlidesBounds: true,
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.room-next',
+        prevEl: '.room-prev',
+    },
+
+});
+
+
+const swiperMenu = new Swiper('.swiperMenu', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            createElement: false,
+            
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.menu-next',
+        prevEl: '.menu-prev',
+    },
+
+});
+
+const swiperFacilities = new Swiper('.swiperFacilities', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    edgeSwipeDetection: false,
+    centeredSlides: true,
+
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 6,
+            spaceBetween: 0,
+            createElement: false,
+        },
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
+
+const swiperImg = new Swiper('.swiperImg', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    edgeSwipeDetection: false,
+
+
+    breakpoints: {
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+            createElement: false,
+        },
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar-first',
+    },
+});
+
+const swiperImgTwo = new Swiper('.swiperImgTwo', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    edgeSwipeDetection: false,
+    
+    breakpoints: {
+        320:{
+            centeredSlides: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            centerInsufficientSlides:true,
+
+        },
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            centeredSlidesBounds: true,
+            createElement: false,
+        },
+    },
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
