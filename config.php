d028865c346c5ecd0fb7c6b328bc31f78aa692cd
Federico Iglesias <igl.fede@gmail.com>
2023-06-02 09:55:44 +0200

    conectDB

--- config.php
+++ config.php
@@ -6,3 +6,18 @@ use eftec\bladeone\BladeOne;
 $views = __Dir__ . '/view';
 $cache = __Dir__ . '/cache';
 $blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG);
+
+
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// ?>
