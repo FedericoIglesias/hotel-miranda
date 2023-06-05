be146d3c8047a945e458ccc7448035cd97906903
Federico Iglesias <igl.fede@gmail.com>
2023-06-05 16:11:31 +0200

    createRoutes

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-require_once 'config.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 
-echo $blade->run("index");
+$routes = [
+    '/' => 'pages/index.php',
+    'about-us' => 'pages/about-us.php',
+    '/rooms' => 'pages/rooms.php',
+    '/offers'  => 'pages/offers.php',
+    '/new-details' => 'pages/new-details.php',
+];
+
+
+if(array_key_exists($uri,$routes)){
+    require $routes[$uri];
+}else{
+    require $routes['/'];
+}
