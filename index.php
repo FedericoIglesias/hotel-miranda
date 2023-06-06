04d3ac2e899cafe5f5debad273c922ab501bcf5c
Federico Iglesias <igl.fede@gmail.com>
2023-06-06 16:52:28 +0200

    conectDB

--- index.php
+++ index.php
@@ -5,15 +5,31 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
     '/' => 'pages/index.php',
-    'about-us' => 'pages/about-us.php',
+    '/about-us' => 'pages/about-us.php',
     '/rooms' => 'pages/rooms.php',
     '/offers'  => 'pages/offers.php',
     '/new-details' => 'pages/new-details.php',
 ];
 
 
-if(array_key_exists($uri,$routes)){
-    require $routes[$uri];
-}else{
-    require $routes['/'];
+// if(array_key_exists($uri,$routes)){
+//     require $routes[$uri];
+// }else{
+//     require $routes['/'];
+// }
+
+$configDB = 'mysql:host=localhost;port=3306;dbname=hotel_miranda;user=root;password=1234;charset=utf8';
+
+$db = new PDO($configDB);
+
+$statament = $db -> prepare('select * from rooms');
+
+$statament -> execute();
+
+$posts = $statament -> fetchAll(PDO::FETCH_ASSOC);
+
+// echo(implode('',$posts));
+
+foreach($posts as $post){
+    echo implode(' ',$post) . "<br/>\n";
 }
